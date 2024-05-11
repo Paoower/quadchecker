8b6ab7dd567939ab4f62a3951b03b9436a338a4e
lmarchet <lucasmarchetti21@gmail.com>
2024-05-11 13:15:55 +0200

    updated quadchecker

--- pre_build/quadchecker.go
+++ pre_build/quadchecker.go
@@ -10,11 +10,13 @@ import (
 	"strings"
 )
 
+var isStarting bool = false
+
 func main() {
-	// Lire la sortie de l'exécutable précédent à partir de l'entrée standard
+	// Lire la sortie de l'executable precedent à partir de l'entree standard
 	input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		fmt.Println("Erreur lors de la lecture de l'entrée standard :", err)
+		fmt.Println("Erreur lors de la lecture de l'entree standard :", err)
 		os.Exit(1)
 	}
 	targetOutput := string(input)
@@ -26,39 +28,44 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Extraire les valeurs de x et y à partir des chaînes de caractères formatées avec %!s(int=...)
+	// Extraire les valeurs de x et y à partir des chaînes de caractères formatees avec %!s(int=...)
 	xStr := strings.Trim(string(data[:len(data)/2]), "%!s(int=) ")
 	xInt, err := strconv.Atoi(xStr)
 	if err != nil {
-		fmt.Println("Erreur lors de l'extraction de la valeur de x à partir de la chaîne de caractères formatée :", err)
+		fmt.Println("Erreur lors de l'extraction de la valeur de x à partir de la chaîne de caractères formatee :", err)
 		os.Exit(1)
 	}
 	yStr := strings.Trim(string(data[len(data)/2:]), "%!s(int=) \n")
 	yInt, err := strconv.Atoi(yStr)
 	if err != nil {
-		fmt.Println("Erreur lors de l'extraction de la valeur de y à partir de la chaîne de caractères formatée :", err)
+		fmt.Println("Erreur lors de l'extraction de la valeur de y à partir de la chaîne de caractères formatee :", err)
 		os.Exit(1)
 	}
 
-	// Définir les noms des exécutables
-	executables := []string{"./quadA", "./quadB", "./quadC", "./quadD", "./quadE"}
+	// Definir les noms des executables
+	executables := []string{"quadA", "quadB", "quadC", "quadD", "quadE"}
 
-	// Exécuter les autres exécutables et comparer la sortie
+	// Executer les autres executables et comparer la sortie
 	for _, execName := range executables {
-		// Exécuter l'exécutable avec les valeurs de x et y extraites
-		cmd := exec.Command(execName, strconv.Itoa(xInt), strconv.Itoa(yInt))
+		// Executer l'executable avec les valeurs de x et y extraites
+		cmd := exec.Command("./"+execName, strconv.Itoa(xInt), strconv.Itoa(yInt))
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		err := cmd.Run()
 		if err != nil {
-			fmt.Printf("Erreur lors de l'exécution de %s : %v\n", execName, err)
+			fmt.Printf("Erreur lors de l'execution de %s : %v\n", execName, err)
 			continue
 		}
 		output := out.String()
 
 		// Comparer la sortie avec la sortie cible en utilisant strings.EqualFold
 		if strings.EqualFold(targetOutput, output) {
-			fmt.Printf("[%s] [%d] [%d] || ", execName, xInt, yInt)
+			if !isStarting {
+				fmt.Printf("[%s] [%d] [%d]", execName, xInt, yInt)
+				isStarting = true
+			} else {
+				fmt.Printf(" || [%s] [%d] [%d]", execName, xInt, yInt)
+			}
 		}
 	}
 	fmt.Println()
