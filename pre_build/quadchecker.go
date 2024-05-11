f68857393d694c176b703737d14243017e30f398
lmarchet <lucasmarchetti21@gmail.com>
2024-05-11 15:45:51 +0200

    cleaning

--- pre_build/quadchecker.go
+++ pre_build/quadchecker.go
@@ -10,58 +10,54 @@ import (
 	"strings"
 )
 
-var isStarting bool = false
+var isStarting bool = false // Variable globale permettant de gérer les séparateurs "||" dans le résultat.
 
 func main() {
-	// Lire la sortie de l'executable precedent à partir de l'entree standard
+	originalOutput := OutputOfExec() // Met dans une variable la sortie standard du résultat de la fonction en type string.
+	txtData, err := ReadQuadTxt()    // Ouverture du fichier quad-x-y.txt généré par l'executable quad[A-E] pour transmettre les coordonnées x et y à quadchecker.
+	if err != nil {
+		fmt.Println("Not a quad function") // Si le fichier n'est pas trouvé, c'est que ça n'est pas un quad[A-E]
+		return
+	}
+	fmt.Println(originalOutput)                                          // Print de la sortie du quad. ex: "o---o"
+	xInt, yInt := XYExtract(txtData)                                     // Extraire les valeurs de x et y à partir des chaînes de caractères formatees avec %!s(int=...) + conversion string to int(Atoi).
+	executables := []string{"quadA", "quadB", "quadC", "quadD", "quadE"} // Definir les noms des executables
+	ExecAllQuads(xInt, yInt, executables, originalOutput)                // Executer les autres executables et comparer la sortie
+	os.Remove("quad-x-y.txt")                                            // Supprimer le fichier quad-x-y.txt
+}
+
+func OutputOfExec() string {
 	input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Println("Erreur lors de la lecture de l'entree standard :", err)
 		os.Exit(1)
 	}
-	targetOutput := string(input)
+	return string(input)
+}
 
-	// Lire les valeurs de x et y à partir du fichier quad-x-y.txt
-	data, err := ioutil.ReadFile("quad-x-y.txt")
-	if err != nil {
-		fmt.Println("Not a quad function")
-		return
-	}
+func ReadQuadTxt() ([]byte, error) {
+	txtData, err := ioutil.ReadFile("quad-x-y.txt")
+	return txtData, err
+}
 
-	fmt.Println(targetOutput)
+func XYExtract(txtData []byte) (int, int) {
+	xStr := strings.Trim(string(txtData[:len(txtData)/2]), "%!s(int=) ")   // extrait x de la chaîne "%!s(int=x) "
+	xInt, _ := strconv.Atoi(xStr)                                          // Conversion de x type string à x type int.
+	yStr := strings.Trim(string(txtData[len(txtData)/2:]), "%!s(int=) \n") // extrait y de la chaîne "%!s(int=y) "
+	yInt, _ := strconv.Atoi(yStr)                                          // Conversion de y type string à y type int.
+	return xInt, yInt
+}
 
-	// Extraire les valeurs de x et y à partir des chaînes de caractères formatees avec %!s(int=...)
-	xStr := strings.Trim(string(data[:len(data)/2]), "%!s(int=) ")
-	xInt, err := strconv.Atoi(xStr)
-	if err != nil {
-		fmt.Println("Erreur lors de l'extraction de la valeur de x à partir de la chaîne de caractères formatee :", err)
-		os.Exit(1)
-	}
-	yStr := strings.Trim(string(data[len(data)/2:]), "%!s(int=) \n")
-	yInt, err := strconv.Atoi(yStr)
-	if err != nil {
-		fmt.Println("Erreur lors de l'extraction de la valeur de y à partir de la chaîne de caractères formatee :", err)
-		os.Exit(1)
-	}
-
-	// Definir les noms des executables
-	executables := []string{"quadA", "quadB", "quadC", "quadD", "quadE"}
-
-	// Executer les autres executables et comparer la sortie
+func ExecAllQuads(xInt, yInt int, executables []string, originalOutput string) {
 	for _, execName := range executables {
 		// Executer l'executable avec les valeurs de x et y extraites
 		cmd := exec.Command("./"+execName, strconv.Itoa(xInt), strconv.Itoa(yInt))
 		var out bytes.Buffer
 		cmd.Stdout = &out
-		err := cmd.Run()
-		if err != nil {
-			fmt.Printf("Erreur lors de l'execution de %s : %v\n", execName, err)
-			continue
-		}
+		cmd.Run()
 		output := out.String()
-
 		// Comparer la sortie avec la sortie cible en utilisant strings.EqualFold
-		if strings.EqualFold(targetOutput, output) {
+		if strings.EqualFold(originalOutput, output) {
 			if !isStarting {
 				fmt.Printf("[%s] [%d] [%d]", execName, xInt, yInt)
 				isStarting = true
@@ -70,12 +66,5 @@ func main() {
 			}
 		}
 	}
-	fmt.Println()
-
-	// Supprimer le fichier quad-x-y.txt
-	err = os.Remove("quad-x-y.txt")
-	if err != nil {
-		fmt.Println("Erreur lors de la suppression du fichier :", err)
-		os.Exit(1)
-	}
+	fmt.Println() // Saut de ligne.
 }
