e1c2e0d9cdf018d3a3c2c2e3d34e3ad334dad445
lmarchet <lucasmarchetti21@gmail.com>
2024-05-13 14:14:20 +0200

    update Itoa, Atoi removed

--- pre_build/quadchecker.go
+++ pre_build/quadchecker.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"strconv"
 	"strings"
 )
 
@@ -20,9 +19,9 @@ func main() {
 		return
 	}
 	fmt.Println(originalOutput)                                          // Print de la sortie du quad. ex: "o---o"
-	xInt, yInt := XYExtract(txtData)                                     // Extraire les valeurs de x et y à partir des chaînes de caractères formatees avec %!s(int=...) + conversion string to int(Atoi).
+	x, y := XYExtract(txtData)                                           // Extraire les valeurs de x et y à partir des chaînes de caractères formatees avec %!s(int=...) + conversion string to int(Atoi).
 	executables := []string{"quadA", "quadB", "quadC", "quadD", "quadE"} // Definir les noms des executables
-	ExecAllQuads(xInt, yInt, executables, originalOutput)                // Executer les autres executables et comparer la sortie
+	ExecAllQuads(x, y, executables, originalOutput)                      // Executer les autres executables et comparer la sortie
 	os.Remove("quad-x-y.txt")                                            // Supprimer le fichier quad-x-y.txt
 }
 
@@ -36,18 +35,16 @@ func ReadQuadTxt() ([]byte, error) {
 	return txtData, err
 }
 
-func XYExtract(txtData []byte) (int, int) {
-	xStr := strings.Trim(string(txtData[:len(txtData)/2]), "%!s(int=) ")   // extrait x de la chaîne "%!s(int=x) "
-	xInt, _ := strconv.Atoi(xStr)                                          // Conversion de x type string à x type int.
-	yStr := strings.Trim(string(txtData[len(txtData)/2:]), "%!s(int=) \n") // extrait y de la chaîne "%!s(int=y) "
-	yInt, _ := strconv.Atoi(yStr)                                          // Conversion de y type string à y type int.
-	return xInt, yInt
+func XYExtract(txtData []byte) (string, string) {
+	x := strings.Trim(string(txtData[:len(txtData)/2]), "%!s(int=) ")   // extrait x de la chaîne "%!s(int=x) "
+	y := strings.Trim(string(txtData[len(txtData)/2:]), "%!s(int=) \n") // extrait y de la chaîne "%!s(int=y) "
+	return x, y
 }
 
-func ExecAllQuads(xInt, yInt int, executables []string, originalOutput string) {
+func ExecAllQuads(x, y string, executables []string, originalOutput string) {
 	for _, execName := range executables {
 		// Executer l'executable avec les valeurs de x et y extraites
-		cmd := exec.Command("./"+execName, strconv.Itoa(xInt), strconv.Itoa(yInt))
+		cmd := exec.Command("./"+execName, x, y)
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		cmd.Run()
@@ -55,10 +52,10 @@ func ExecAllQuads(xInt, yInt int, executables []string, originalOutput string) {
 		// Comparer la sortie avec la sortie cible en utilisant strings.EqualFold
 		if strings.EqualFold(originalOutput, output) {
 			if !isStarting {
-				fmt.Printf("[%s] [%d] [%d]", execName, xInt, yInt)
+				fmt.Printf("[%s] [%s] [%s]", execName, x, y)
 				isStarting = true
 			} else {
-				fmt.Printf(" || [%s] [%d] [%d]", execName, xInt, yInt)
+				fmt.Printf(" || [%s] [%s] [%s]", execName, x, y)
 			}
 		}
 	}
